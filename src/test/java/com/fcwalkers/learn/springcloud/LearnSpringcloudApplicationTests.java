2047c0b4b320ac87b5151eee38e3002dc620af3d
Windus <huizhimrl@gmail.com>
2020-10-14 17:59:50 +0800

    first commit

---
+++ src/test/java/com/fcwalkers/learn/springcloud/LearnSpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcwalkers.learn.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
